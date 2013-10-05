4aa705a59e5818a4342bb12736f2425106a5c8f1
Albert Toledo <toledoalbert@gmail.com>
2013-10-05 16:08:57 -0400

    xml modification for test

---
+++ parser.php
@@ -0,0 +1,17 @@
+<?php 
+
+	//xml parser
+	//load the topics.xml file
+	$xml simplexml_load_file('topics.xml');
+
+
+	//echo $xml;
+	//$xml = simplexml_load_string('topics.xml');
+	//print_r($xml);
+	//$num = 'dasdasddasdasd';
+	//echo $xml->top[1];
+	//echo $num;
+	//echo $xml->top[0]->desc;
+	//echo "dasdas";
+
+?>
