84400aff976820e579916836859837728047d63b
Albert Toledo <toledoalbert@gmail.com>
2013-10-08 12:59:21 -0400

    shit not working

--- freqMatrix.php
+++ freqMatrix.php
@@ -1,6 +1,6 @@
 <?php
 
-	//connection
+//connection
 $con = mysql_connect("localhost","root","root");
 
 	//check if connection successful
@@ -28,19 +28,65 @@ for($i = 351; $i < 401; $i++){
 	//put all the fields together to one big string
 	$all = $row['title']. " " . $row['description'] . " " . $row['narrative'];
 
+	//trim the string
+	$all = trim($all);
+
 	//put all the words in an array
 	$all = explode(" ", $all);
 
-	//remove every word from array if it is a stopword
+	//load list of common words
+ 	$stopwords = file('stopwords.txt');
+ 	//$stopwords = array('is', "the");
+
+	/*remove every word from array if it is a stopword
 	foreach( $all as $key => $val ) {
 
-        if( in_array($val, $stopwords) ) {
-            unset($all[$key]);    
-        }
-    }
+		//trim the word before matching
+		$val = trim($val);
 
-    //add a new a column with doc number and 
+		if( in_array($val, $stopwords) ) {
+			unset($all[$key]);    
+		}
+	}*/
+
+	//remove the stop words from the array of words
+	$pure = array_diff($all, $stopwords);
+
+	//print_r($pure);
+
+	//echo implode(" ", $pure);
+
+	//add a new column for each document
+	$query = "ALTER TABLE  `freq` ADD  `$i` VARCHAR( 255 ) NOT NULL";
+
+	$result = mysql_query($query, $con);
+
+	//count the occurances of the words in the document
+	$counts = array_count_values($pure);
+
+	//print_r($counts); 
+
+	//for each word insert a row
+	foreach($counts as $key => $val){
+
+		addslashes($key);
+		addslashes($val);
+
+		//query to insert the info
+		$query = "INSERT INTO freq (word, '$i') VALUES ('$key', '$val')";
+
+		//run the query
+		$result = mysql_query($con, $query);
+
+		//if($result){ echo "success";}else{echo mysql_error();}
+
+		//echo "</br></br>";
+
+	}
 
 }
+
+//
+
 
 ?>
---
+++ removeStop.php
@@ -0,0 +1,17 @@
+<?php
+
+	/*$stopwords = array('in', 'the', 'a', 'to', 'I');
+
+	$all = 'I went to the market to get a choacalate.';
+
+	$all = explode(" ", $all);
+
+	$all = array_diff($all, $stopwords);
+
+	$all = implode(" ", $all);
+
+	echo $all;*/
+
+	
+
+?>
