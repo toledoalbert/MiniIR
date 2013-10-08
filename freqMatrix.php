ef283347e8cdbe7cf1af6aeba0420dfb325a8715
Albert Toledo <toledoalbert@gmail.com>
2013-10-07 21:50:20 -0400

    get rid of echo statements for testing

--- freqMatrix.php
+++ freqMatrix.php
@@ -18,32 +18,18 @@ echo 'entering the loop</br>';
 //get the articles from the docs table
 for($i = 351; $i < 401; $i++){
 
-	echo 'processing number '.$i.'</br>';
-
 	//write the sql query to select the row of that single article
 	$query = "SELECT * FROM docs WHERE num='$i'";
-
-	echo 'sql query = '.$query.'</br>';
 
 	//submit the mysql query
 	$result = mysql_query($query, $con);
 
-	echo $result.'</br>';
-
 	//put the row in an array
 	$row = mysql_fetch_array($result);
 
-	echo $row.'</br>';
-
 	//put all the fields together to one big string
 	$all = $row['title'] + " " + $row['description'] + " " + $row['narrative'];
-	//echo $row['title']; 
-	//echo $row['desc']; 
-	echo $row['narrative'];
 
-	//echo $row['title'] + ;
-
-	//echo $all.'</br>';
 
 
 }
